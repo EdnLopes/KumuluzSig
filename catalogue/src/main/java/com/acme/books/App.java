d227ca085bbf8be2ca7f4486aefcea840a0e7bbe
ednaldo.silva <ednaldo.silva@redetendencia.com.br>
2016-09-16 16:07:04 -0400

    #Desenv

---
+++ catalogue/src/main/java/com/acme/books/App.java
@@ -0,0 +1,15 @@
+package com.acme.books;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        
+    }
+}
